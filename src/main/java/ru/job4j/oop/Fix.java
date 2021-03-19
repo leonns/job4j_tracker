d6580f1241c053560be0eb6bf81e160c31ec8bec
Lev <lpkhor@mail.ru>
2021-03-19 20:22:32 +0300

    1.1.1. Конструктор по умолчанию [#320989]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String text) {
+        this.desc = text;
+    }
+
+    public static void main(String[] args) {
+        String issue = "Fix me";
+        Fix fix = new Fix(issue);
+    }
+}
