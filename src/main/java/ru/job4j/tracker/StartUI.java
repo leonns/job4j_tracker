75d510bc89f7d42181094fa413d130e096603553
Lev <lpkhor@mail.ru>
2021-04-13 20:05:03 +0300

    7. Date. Отображение даты. [#320992]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        LocalDateTime currentDateTimeFormat = item.getCreated();
+        String current = currentDateTimeFormat.format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + current);
+
+    }
+
+}
